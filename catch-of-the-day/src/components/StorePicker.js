659f16dc0d5f9c091c733a999e856266de072504
Niklas Heer <niklas.heer@gmail.com>
2018-12-07 17:13:27 +0100

    Add React Fragment

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
-    render() {
-        return <p>I am the store picker!</p>
-    }
+  render() {
+    return (
+      <Fragment>
+        <p>Fish!</p>
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+        </form>
+      </Fragment>
+    )
+  }
 }
 
 export default StorePicker;
