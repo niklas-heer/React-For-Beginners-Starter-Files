782ff39180a44e9dccc1ee8e257d79be373166e9
Niklas Heer <niklas.heer@gmail.com>
2018-12-09 17:55:46 +0100

    Add code from video 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
-class App extends React.Component {
+export default class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+
+    // 2. Add our new fish to our fishes
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3. Set the new fishes object to state
+    this.setState({
+      fishes // fishes: fishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,10 +29,8 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
-    )
+    );
   }
 }
-
-export default App;
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,13 +1,13 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
-class Inventory extends React.Component {
-	render () {
-		return (
+export default class Inventory extends React.Component {
+  render() {
+    return (
       <div className="inventory">
-        Inventory
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
       </div>
-    )
-	}
+    );
+  }
 }
-
-export default Inventory
